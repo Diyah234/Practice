4d9ddf155a6a22832a929d7fd7e962e39db6ed61
Abudu Rodiyyah <rodiyahabudu@gmail.com>
2023-08-13 15:43:43 +0100

    second update

--- fashion nova/nova.js
+++ fashion nova/nova.js
@@ -25,12 +25,51 @@
 //     }
 //   });
 
-document.body.addEventListener("scroll", function(){
-    let Navbar = document.getElementById('navbar');
-    if(document.documentElement.scrollTop > 0){
-      Navbar.classList.add("navbar-scroll");
-    }else{
-      Navbar.classList.remove("navbar-scroll");
-    }
-  });
+// document.body.addEventListener("scroll", function(){
+//     let Navbar = document.getElementById('navbar');
+//     if(document.documentElement.scrollTop > 0){
+//       Navbar.classList.add("navbar-scroll");
+//     }else{
+//       Navbar.classList.remove("navbar-scroll");
+//     }
+//   });
   
+  window.onscroll = ()=> {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      document.getElementById("navbar").className = "navbar-scroll";
+    } else if(document.body.scrollTop < 45 || document.documentElement.scrollTop < 45) {
+      document.getElementById("navbar").className ="navbar-show";
+    }
+    else{
+      document.getElementById("navbar").className ="";
+    }
+  }
+
+
+  // window.onscroll = function() {myFunction()};
+
+  // function myFunction() {
+  //   if (document.documentElement.scrollTop > 200) {
+  //     document.getElementById("grid").className = "slideUp";
+  //   }
+  // }
+
+let count = 0;
+document.getElementById("decrease").onclick = function(){
+   count -=1;
+   document.getElementById("count").innerHTML = count;
+
+   if (count < 0) {
+    count = 0;
+    document.getElementById("count").innerHTML = count;
+  }
+}
+
+document.getElementById("increase").onclick = function(){
+  count +=1;
+   document.getElementById("count").innerHTML = count;
+}
+
+document.querySelector(".btn").addEventListener("click" , function(){
+  document.querySelector("footer")
+})
