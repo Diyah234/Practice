36d897a02f51993c5e5a4aacb7b91805bb9dbd47
Abudu Rodiyyah <rodiyahabudu@gmail.com>
2023-08-17 14:54:50 +0100

    another commit

--- fashion nova/nova.js
+++ fashion nova/nova.js
@@ -68,8 +68,37 @@ document.getElementById("decrease").onclick = function(){
 document.getElementById("increase").onclick = function(){
   count +=1;
    document.getElementById("count").innerHTML = count;
+   document.getElementById("items").innerHTML = count + " items"
 }
 
-document.querySelector(".btn").addEventListener("click" , function(){
-  document.querySelector("footer")
-})
+// document.querySelector("img").addEventListener("click" , function(){
+//   document.querySelector("footer").style.display = "block";
+// })
+
+function clicking(){
+  document.querySelector("footer").style.display = "block";
+}
+
+// menubar
+$(document).ready(function(){
+	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+		$(this).toggleClass('open');
+	});
+});
+
+const menuButton = document.getElementById("nav-icon3");
+const navBar = document.getElementById("nav-bar");
+
+menuButton.addEventListener("click", function() {
+  if (navBar.style.display === "block") {
+    navBar.style.display = "none"; // Hide the navigation menu
+  } else {
+    navBar.style.display = "block"; // Show the navigation menu
+  }
+});
+
+document.querySelector(".cart").onclick= function(){
+  document.getElementById("footer1").style.display = "none";
+  document.getElementById("footer2").style.display = "block";
+
+};
